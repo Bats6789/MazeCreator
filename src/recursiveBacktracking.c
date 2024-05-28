bd8ea9c20a3fa6b78c9505a6f0b17c8f6de1b1c1
Blake Wingard <bats23456789@gmail.com>
2024-05-28 16:32:44 -0500

    Fixed bugs in wilson and recursive backtracking.

--- src/recursiveBacktracking.c
+++ src/recursiveBacktracking.c
@@ -13,7 +13,7 @@ static void recursive(Maze_t *maze, Point_t point) {
 
     for (size_t i = 0; i < dirSz; i++) {
         Point_t newPoint = pointShift(point, dir[i]);
-        size_t newIndex = pointToIndex(point, maze->width);
+        size_t newIndex = pointToIndex(newPoint, maze->width);
         if (!maze->cells[newIndex].visited) {
 			mazeConnectCells(maze, index, newIndex, dir[i]);
             maze->cells[newIndex].visited = 1;
@@ -34,7 +34,7 @@ static void recursiveWithSteps(Maze_t *maze, Point_t point,
 
     for (size_t i = 0; i < dirSz; i++) {
         Point_t newPoint = pointShift(point, dir[i]);
-        size_t newIndex = pointToIndex(point, maze->width);
+        size_t newIndex = pointToIndex(newPoint, maze->width);
         if (!maze->cells[newIndex].visited) {
 			mazeConnectCells(maze, index, newIndex, dir[i]);
             maze->cells[newIndex].visited = 1;
